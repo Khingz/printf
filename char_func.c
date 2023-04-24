741b2f2c08dd8fbb9417a11ab044787d70824692
Khingz <kingsley995mr@gmail.com>
2023-04-24 14:13:51 -0700

    created char_func and string_func files

---
+++ char_func.c
@@ -0,0 +1,10 @@
+/**
+ * char_func - print char to standard output
+ * @ch: character to print
+ * Return: 1
+ */
+
+int char_func(va_list ch)
+{
+	return (-1);
+}
---
+++ string_func.c
@@ -0,0 +1,10 @@
+/**
+ * string_func - print a string to standard output
+ * @str: string to be printed
+ * Return: count
+ */
+
+int string_func(va_list str)
+{
+	return (0);
+}
