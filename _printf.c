b7662aa7ed730553d4b8f2171997cf7968f4cd71
Khingz <kingsley995mr@gmail.com>
2023-04-24 12:47:35 -0700

    simple printf without specifiers args

--- _printf.c
+++ _printf.c
@@ -4,12 +4,23 @@ int _printf(const char *format, ...)
 {
 	va_list params;
 	int (*sp_f)(va_list);
+	int i = 0, count = 0;
 	
 	if (format == NULL)
 	{
 		return (-1);
 	}
 	va_start(params, format);
+	while (format[i])
+	{
+		if (format[i] && format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+			i++;
+			continue;
+		}
+	}
 
 	va_end(params);
 	return (0);
