289120aa1bfd34733bc9101f993faa68ee9bb186
Khingz <kingsley995mr@gmail.com>
2023-04-25 07:15:15 -0700

    created int_dec_func

---
+++ int_dec_func.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* int_dec_func - prints integers and decimal numbers
+* @val: integer or decimal to print
+* Return: number of integer
+*/
+int int_dec_func(va_list val)
+{
+	return (0);
+}
