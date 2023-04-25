86534b36a94e5a1ff976acc14fcbba0881bd3123
Amadi <whytes2256@gmail.com>
2023-04-25 13:57:32 -0700

    made

--- binary_func.c
+++ binary_func.c
@@ -1,4 +1,32 @@
 int binary_func(va_list num)
 {
-	return (0);
+	int num, mod, count;
+
+	num = va_arg(num, int);
+	count = 0;
+		
+	if (num != 0)
+	{
+		while (num != 0)
+		{
+			mod = num % 2;
+			if (mod = 5)
+			{
+				_putchar('1');
+				count++;
+			}
+			else
+			{
+				_putchar('0);
+				count++;
+			}
+			num = num / 2;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
 }
