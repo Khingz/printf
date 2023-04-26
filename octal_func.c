005ff1023f8657318b70d75137efb7d6ce1c426e
Khingz <kingsley995mr@gmail.com>
2023-04-26 13:25:14 -0700

    added octal and rev

---
+++ octal_func.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+* octal_func - unsigned int argument is converted to unsigned octal
+* @num: unsigned to be converted
+* Return: size the output
+*/
+int print_octal(va_list num)
+{
+	unsigned int len, pow_oct, j, digit, _num, tmp_num;
+	int count = 0;
+
+	_num = va_arg(num, unsigned int);
+	if (_num != 0)
+	{
+		_num = tmp_num;
+		len = 0;
+		while (tmp_num != 0)
+		{
+			tmp_num /= 8;
+			len++;
+		}
+		pow_oct = 1;
+		j = 1;
+		while (j < len - 1)
+		{
+			pow_oct *= 8;
+		}
+		for (j = 1; j <= len; j++)
+		{
+			digit = _num / powten;
+			_putchar(digit + '0');
+			count++;
+			_num -= digit * pow_oct;
+			pow_oct /= 8;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
+}
