bf04e100997f7b83c17dfde5168626166e358d08
Amadi <whytes2256@gmail.com>
2023-04-26 13:08:40 -0700

    edited get_specifier_func file

--- main.h
+++ main.h
@@ -15,6 +15,7 @@ int (*get_specifier_func(const char *specifier))(va_list);
 int percent_func(va_list args);
 int int_dec_func(va_list val);
 int binary_func(va_list num);
+int rot_func(va_list str);
 
 /* struct definition */
 /**
