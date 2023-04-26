a26791f439b5ad9ac4a51d3654000c9f1982b5f7
Khingz <kingsley995mr@gmail.com>
2023-04-26 06:43:57 -0700

    fixing bin function

--- binary_func.c
+++ binary_func.c
@@ -1,29 +1,18 @@
 #include "main.h"
 
+int int_to_bin(unsigned int num);
+
 int binary_func(va_list n)
 {
-	int num, mod, count;
+	int count;
+	unsigned int num;
 
 	num = va_arg(n, int);
 	count = 0;
-		
+	
 	if (num != 0)
 	{
-		while (num != 0)
-		{
-			mod = num % 2;
-			if (mod % 2 == 0)
-			{
-				_putchar('0');
-				count++;
-			}
-			else
-			{
-				_putchar('1');
-				count++;
-			}
-			num = num / 2;
-		}
+		count = int_to_bin(num);
 	}
 	else
 	{
@@ -31,4 +20,21 @@ int binary_func(va_list n)
 		return (1);
 	}
 	return (count);
+}
+
+/*
+ *
+ */
+int int_to_bin(unsigned int num)
+{
+	int count;
+
+	count = 0;
+	if (num > 1)
+	{
+		int_to_bin(num / 2);
+	}
+	count++;
+	_putchar((num % 2) + '0');
+	return count;
 }
